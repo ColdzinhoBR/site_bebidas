292f48a99ff2dbe77a3f3d9a9feb67017f0d82c9
Danlucas7 <danilucas2205@gmail.com>
2025-05-26 02:27:06 -0300

    Adiciona arquivo JS externo e integra com formulário

--- script.js
+++ script.js
@@ -239,3 +239,34 @@ window.addEventListener("hashchange", function () {
         window.scrollTo({ top: y - 15, behavior: "auto" });
     }, 10);
 });
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section[id]");
+    const navLinks = document.querySelectorAll(".category-bar a");
+
+    function activateLinkOnScroll() {
+        let scrollY = window.pageYOffset;
+
+        sections.forEach(current => {
+            const sectionTop = current.offsetTop - 110;
+            const sectionHeight = current.offsetHeight;
+            const sectionId = current.getAttribute("id");
+
+            if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+                navLinks.forEach(link => {
+                    link.classList.remove("active");
+                    if (link.getAttribute("href") === `#${sectionId}`) {
+                        link.classList.add("active");
+                    }
+                });
+            }
+        });
+    }
+
+    window.addEventListener("scroll", activateLinkOnScroll);
+});
